0584c420e0ca54721bfa82d55401c60582a56bdf
micher12 <michelasm3@gmail.com>
2025-06-27 14:53:47 -0300

    update: added social media buttons

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,5 @@
+import { Github, Linkedin, Mail } from "lucide-react";
+import Link from "next/link";
 
 export default function Hero(){
     return(
@@ -6,7 +8,15 @@ export default function Hero(){
                 <h2 className="w-fit animateText text-3xl sm:text-5xl xl:text-7xl font-bold bg-gradient-to-r from-amber-400 via-purple-500 to-pink-400 bg-clip-text text-transparent">Olá, eu sou Michel!</h2>
                 <h2 className="text-3xl xl:text-5xl font-bold italic mt-2">Desenvolvedor FullStack</h2>
                 <p className="text-4xl sm:text-5xl mt-12 max-w-[300px] sm:max-w-[50%] font-semibold italic montserrat">Transformando ideias em experiências digitais excepcionais.</p>
-                <h2 className="cursor-pointer myTransition-ease scale mt-10 font-bold bg-gradient-to-r from-amber-500 via-purple-500 to-pink-500 w-fit px-12 text-white rounded-full py-2 animateText hover:shadow-lg shadow-cyan-800/60">Ver projetos</h2>
+                <div className="flex gap-5 mt-10">
+                    <h2 className="cursor-pointer myTransition-ease scale font-bold bg-gradient-to-r from-amber-500 via-purple-500 to-pink-500 w-fit px-8 sm:px-12 text-white rounded-full py-2 hover:shadow-lg shadow-pink-800/60">Ver projetos</h2>
+                    <h2 className="cursor-pointer myTransition-ease scale font-bold bg-gradient-to-r from-slate-800 via-indigo-500 to-blue-500/30 w-fit px-8 sm:px-12 text-white rounded-full py-2 hover:shadow-lg shadow-blue-800/60">Sobre</h2>
+                </div>
+                <div className="flex gap-3 mt-10">
+                    <Link target="_blank" href={"https://www.github.com/micher12"} className="hover:bg-zinc-500 p-1.5 rounded-full myTransitionPop  scale-up" ><Github className="w-6 h-6" /></Link>
+                    <Link target="_blank" href={"https://www.linkedin.com/in/michel-alves-da-silva-0a1834212/"} className="hover:bg-zinc-500 p-1.5 rounded-full myTransitionPop  scale-up" ><Linkedin className="w-6 h-6" /></Link>
+                    <Link target="_blank" href={"mailto:michelasm3@gmail.com?subject=Contato&body=Olá vim pelo website, "} className="hover:bg-zinc-500 p-1.5 rounded-full myTransitionPop scale-up" ><Mail className="w-6 h-6" /></Link>
+                </div>
             </div>
         </main>
     )
